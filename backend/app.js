7af4610c4a60f026908bde3a86710631b7bccb44
Maxence Eudier <eudier.maxence@gmail.com>
2021-12-06 09:57:11 +0100

    connection bdd

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//connection BDD
+var connectionBDD = require('./modules/coonectionBDD');
+
 var app = express();
 
 // view engine setup
