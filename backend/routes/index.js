3e4e8452e2372ee8a42d4e8be4bfbcdcc8392003
Maxence Eudier <eudier.maxence@gmail.com>
2021-12-07 09:24:29 +0100

    route  search mission

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//format date
+function formatDate (date){
+  return ('0'+date.getDate()).slice(-2)+'/'+ ('0'+parseInt(date.getMonth()+1)).slice(-2)+'/'+date.getFullYear();
+};
 
 //import des modeles
 var userModel = require('../modules/users');
@@ -12,12 +16,22 @@ var deliveryModel = require( '../modules/deliveries');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-  var user = await deliveryModel.findById('61ade704aa1d49805ebbd627').populate('expeditor_id').exec();
-  
-  console.log(user.expeditor_id)
-
   res.render('index', { title: 'Express' });
 });
+
+router.post('/searchKryer', async function(req,res,next){
+
+  var missionList = await missionModel.find({departure_journey:req.body.departure,arrival_journey:req.body.arrival});
+  console.log(missionList);
+  //missionList = missionList.filter(e => e.date_journey >= req.body.date);
+  
+  var result = false;
+  if(missionList){
+    result = missionList;
+  }
+
+  res.json(result)
+})
 
 
 module.exports = router;
