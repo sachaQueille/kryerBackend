02f120102447d54d8f0c72defa51e7f05b595044
Sacha <s.hauberdon@gmail.com>
2021-12-07 08:10:09 +0100

    format date string ok

--- backend/modules/missions.js
+++ backend/modules/missions.js
@@ -1,21 +1,20 @@
-var mongoose = require ('mongoose');
+var mongoose = require("mongoose");
 
 var missionSchema = mongoose.Schema({
-    delivery_id:[{type: mongoose.Schema.Types.ObjectId, ref:'deliveries'}],
-    transport_capacity_total:Number,
-    transport_capacity_rest:Number,
-    date_delivery:Date,
-    place_delivery:Date,
-    date_receipt:Date,
-    place_receipt:Date,
-    pricePerKg:Number,
-    departure_journey:String,
-    arrival_journey:String,
-    date_journey:Date,
-    sattus_mission:String
+  delivery_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "deliveries" }],
+  transport_capacity_total: Number,
+  transport_capacity_rest: Number,
+  date_delivery: String,
+  place_delivery: String,
+  date_receipt: String,
+  place_receipt: String,
+  pricePerKg: Number,
+  departure_journey: String,
+  arrival_journey: String,
+  date_journey: String,
+  sattus_mission: String,
 });
 
-var missionModel = mongoose.model('missions', missionSchema);
-
+var missionModel = mongoose.model("missions", missionSchema);
 
 module.exports = missionModel;
