9cdc775d22a3fc046da5309739aa0edf3bad2778
Sacha <s.hauberdon@gmail.com>
2021-12-07 07:58:28 +0100

    saveMission ok

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
 //import des modeles
-var userModel = require('../modules/users');
-var messageModel = require('../modules/messages');
-var missionModel = require( '../modules/missions');
-var deliveryModel = require( '../modules/deliveries');
-
+var userModel = require("../modules/users");
+var messageModel = require("../modules/messages");
+var missionModel = require("../modules/missions");
+var deliveryModel = require("../modules/deliveries");
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
+  var user = await deliveryModel
+    .findById("61ade704aa1d49805ebbd627")
+    .populate("expeditor_id")
+    .exec();
 
-  var user = await deliveryModel.findById('61ade704aa1d49805ebbd627').populate('expeditor_id').exec();
-  
-  console.log(user.expeditor_id)
+  console.log(user.expeditor_id);
 
-  res.render('index', { title: 'Express' });
+  res.render("index", { title: "Express" });
 });
 
+router.post("/saveMission", async function (req, res, next) {
+  let newMission = new missionModel({
+    date_delivery: req.body.deliveryDate,
+    place_delivery: req.body.deliveryPlace,
+    date_receipt: req.body.recuperationDate,
+    place_receipt: req.body.recuperationPlace,
+    pricePerKg: req.body.pricePerKg,
+    departure_journey: req.body.departure,
+    arrival_journey: req.body.arrival,
+    transport_capacity_total: req.body.weight,
+  });
+
+  await newMission.save();
+
+  res.json({ result: true });
+});
 
 module.exports = router;
