5f090a7014c331e879a76f0a98d8712a449ca97a
Sacha <s.hauberdon@gmail.com>
2021-12-14 08:42:50 +0100

    modif date string

--- backend/modules/messages.js
+++ backend/modules/messages.js
@@ -4,7 +4,7 @@ var messageSchema = mongoose.Schema({
   expeditor_id: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   recipient_id: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   message: String,
-  date: Date,
+  date: String,
 });
 
 var messageModel = mongoose.model("messages", messageSchema);
