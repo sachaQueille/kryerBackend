0cfacd5ccee79a78779db313095a97b1e28e1f50
Maxence Eudier <eudier.maxence@gmail.com>
2021-12-27 12:14:42 +0100

    last version

--- routes/index.js
+++ routes/index.js
@@ -15,18 +15,10 @@ cloudinary.config({
   api_secret: "Qx13Gn87zsegWOrV2PeJcWPSYew",
 });
 
-//format date
-function formatDate(date) {
-  return (
-    ("0" + date.getDate()).slice(-2) +
-    "/" +
-    ("0" + parseInt(date.getMonth() + 1)).slice(-2) +
-    "/" +
-    date.getFullYear()
-  );
-}
+
 
 //import des modeles
+
 var userModel = require("../modules/users");
 var messageModel = require("../modules/messages");
 var missionModel = require("../modules/missions");
@@ -36,125 +28,12 @@ const { discriminator } = require("../modules/users");
 /* GET home page. */
 router.get("/", async function (req, res, next) {
  
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "kryer" });
 });
 
-router.post("/saveMission", async function (req, res, next) {
-  let newMission = new missionModel({
-    date_delivery: req.body.deliveryDate,
-    place_delivery: req.body.deliveryPlace,
-    date_receipt: req.body.recuperationDate,
-    place_receipt: req.body.recuperationPlace,
-    pricePerKg: req.body.pricePerKg,
-    departure_journey: req.body.departure,
-    arrival_journey: req.body.arrival,
-    transport_capacity_total: req.body.weight,
-    transport_capacity_rest: req.body.weight,
-    date_journey: req.body.dateJourney,
-    newMissionStatus: true,
-    currentMissionStatus: false,
-    finishMissionStatus: false,
-    avatarKryer: req.body.avatarKryer,
-    firstNameKryer: req.body.firstNameKryer,
-    lastNameKryer: req.body.lastNameKryer,
-  });
 
-  var missionSave = await newMission.save();
+// authentification
 
-  var user = await userModel.findById(req.body.idKryer);
-  user.missions.push(missionSave._id);
-
- var userSave =  await user.save();
-
-  
-  res.json({ result: userSvae ?  true : false });
-});
-
-router.post("/searchKryer", async function (req, res, next) {
-  var missionList = await missionModel.find({
-    departure_journey: req.body.departure,
-    arrival_journey: req.body.arrival,
-  });
-
-  //missionList = missionList.filter(e => e.date_journey >= req.body.date);
-
-  //filtre sur le poid du colis
-  missionList = missionList.filter(
-    (e) => e.transport_capacity_rest >= req.body.weight
-  );
-
-  missionList = missionList.filter((e) => e.newMissionStatus == true);
-
-
-
-  // je recupere seulement les informations qui m'interessent pour les envoyer dans le front
-  kryerList = [];
-  missionList.map(function (e) {
-    kryerList.push({
-      departure: e.departure_journey,
-      arrival: e.arrival_journey,
-      date: e.date_journey,
-      price: parseInt(e.pricePerKg) * parseInt(req.body.weight),
-      id: e.id,
-      date_delivery: e.date_delivery,
-      place_delivery: e.place_delivery,
-      date_receipt: e.date_receipt,
-      place_receipt: e.place_receipt,
-      infoKryer: {
-        avatar: e.avatarKryer,
-        firstName: e.firstNameKryer,
-        lastName: e.lastNameKryer,
-      },
-    });
-  });
-
-  var result = false;
-  if (kryerList) {
-    result = kryerList;
-  }
-
-  res.json(result);
-});
-
-// router.get("/getMission", async function (req, res, next) {
-//   var missions = await missionModel.find();
-
-//   var result = false;
-//   if (missions) {
-//     result = true;
-//   }
-//   res.json({ result, missions });
-// });
-
-router.post("/signIn", async function (req, res, next) {
-  var result = false;
-  var user = null;
-  var error = [];
-  var token = null;
-
-  if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
-    error.push("champs vides");
-  }
-
-  if (error.length == 0) {
-    user = await userModel.findOne({
-      email: req.body.emailFromFront,
-    });
-
-    if (user) {
-      if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
-        result = true;
-        token = user.token;
-      } else {
-        result = false;
-        error.push("mot de passe incorrect");
-      }
-    } else {
-      error.push("email incorrect");
-    }
-  }
-  res.json({ result, user, error, token });
-});
 
 router.post("/signUp", async function (req, res, next) {
   var error = [];
@@ -201,22 +80,97 @@ router.post("/signUp", async function (req, res, next) {
   res.json({ result, saveUser, error, token });
 });
 
+
+router.post("/signIn", async function (req, res, next) {
+  var result = false;
+  var user = null;
+  var error = [];
+  var token = null;
+
+  if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
+    error.push("champs vides");
+  }
+
+  if (error.length == 0) {
+    user = await userModel.findOne({
+      email: req.body.emailFromFront,
+    });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
+        result = true;
+        token = user.token;
+      } else {
+        result = false;
+        error.push("mot de passe incorrect");
+      }
+    } else {
+      error.push("email incorrect");
+    }
+  }
+  res.json({ result, user, error, token });
+});
+
 // route pour recuperer le user grace au token du storage lors du chargement de l'app
+
 router.get("/getUser", async function (req, res, next) {
   var user = await userModel.find({ token: req.query.token });
 
-  //console.log("user", user);
 
   res.json({ user });
 });
 
-// router.get("/getUserById", async function (req, res, next) {
-//   var user = await userModel.findById(req.query.id);
 
-//   res.json({ user });
-// });
+// changer l'avatar du user
+
+router.post("/updateInfos", async function (req, res, next) {
+  let userExist = await userModel.findOne({ token: req.body.token });
+
+  if (userExist) {
+    userExist.avatar = req.body.avatar;
+    userExist = await userExist.save();
+    res.json(userExist);
+  } else {
+    res.status(500).send("user not found");
+  }
+});
+
+
+
+router.post("/saveMission", async function (req, res, next) {
+  let newMission = new missionModel({
+    date_delivery: req.body.deliveryDate,
+    place_delivery: req.body.deliveryPlace,
+    date_receipt: req.body.recuperationDate,
+    place_receipt: req.body.recuperationPlace,
+    pricePerKg: req.body.pricePerKg,
+    departure_journey: req.body.departure,
+    arrival_journey: req.body.arrival,
+    transport_capacity_total: req.body.weight,
+    transport_capacity_rest: req.body.weight,
+    date_journey: req.body.dateJourney,
+    newMissionStatus: true,
+    currentMissionStatus: false,
+    finishMissionStatus: false,
+    avatarKryer: req.body.avatarKryer,
+    firstNameKryer: req.body.firstNameKryer,
+    lastNameKryer: req.body.lastNameKryer,
+  });
+
+  var missionSave = await newMission.save();
+
+  var user = await userModel.findById(req.body.idKryer);
+  user.missions.push(missionSave._id);
+
+ var userSave =  await user.save();
+
+  
+  res.json({ result: userSave ?  true : false });
+});
+
 
 // route pour save le colis dans bdd
+
 router.post("/saveDelivery", async function (req, res, next) {
   var result = false;
 
@@ -261,27 +215,74 @@ router.post("/saveDelivery", async function (req, res, next) {
   res.json(result);
 });
 
-router.post("/updateInfos", async function (req, res, next) {
-  let userExist = await userModel.findOne({ token: req.body.token });
 
-  if (userExist) {
-    userExist.avatar = req.body.avatar;
-    userExist = await userExist.save();
-    res.json(userExist);
-  } else {
-    res.status(500).send("user not found");
+
+router.post("/searchKryer", async function (req, res, next) {
+  var missionList = await missionModel.find({
+    departure_journey: req.body.departure,
+    arrival_journey: req.body.arrival,
+  });
+
+  //filtre la date
+  missionList = missionList.filter(e => e.date_journey >= req.body.date);
+
+  //filtre sur le poid du colis
+  missionList = missionList.filter(
+    (e) => e.transport_capacity_rest >= req.body.weight
+  );
+
+  //filtre sur le status
+  missionList = missionList.filter((e) => e.newMissionStatus == true);
+
+
+
+  // je recupere seulement les informations qui m'interessent pour les envoyer dans le front (j'aurais pu envoyer missionList et calculer le prix dans le front)
+  kryerList = [];
+  missionList.map(function (e) {
+    kryerList.push({
+      departure: e.departure_journey,
+      arrival: e.arrival_journey,
+      date: e.date_journey,
+      price: parseInt(e.pricePerKg) * parseInt(req.body.weight),
+      id: e.id,
+      date_delivery: e.date_delivery,
+      place_delivery: e.place_delivery,
+      date_receipt: e.date_receipt,
+      place_receipt: e.place_receipt,
+      infoKryer: {
+        avatar: e.avatarKryer,
+        firstName: e.firstNameKryer,
+        lastName: e.lastNameKryer,
+      },
+    });
+  });
+
+  var result = false;
+  if (kryerList) {
+    result = kryerList;
   }
+
+  res.json(result);
 });
+
+
+
+
+
+
 
 router.post("/loadMissions", async function (req, res, next) {
   var result = false;
 
-  var kryer = await userModel
-    .findById(req.body.idKryer)
-    .populate("missions")
-    .exec();
+
+  var kryer = await userModel.find({token:req.body.token})
+  // recupere les missions du user
+  kryer = await userModel.findById(kryer[0]._id).populate("missions").exec();
+
   var missions = kryer.missions;
 
+
+  // filtre les missions en fonction de leur statut selon le bouton sur lequel on press
   if (req.body.status == "newMission") {
     missions = missions.filter((e) => e.newMissionStatus == true);
   } else if (req.body.status == "currentMission") {
@@ -297,34 +298,33 @@ router.post("/loadMissions", async function (req, res, next) {
   res.json(missions);
 });
 
+
+
 router.post("/loadDeliveries", async function (req, res, next) {
   var result = false;
 
+  // recupere tous les colis propre a une mission
   var mission = await missionModel
     .findById(req.body.idMission)
     .populate("delivery_id")
     .exec();
-
   var deliveries = mission.delivery_id;
 
+
+  // filtre des colis selon si la demande est accepté ou pas , si c'est annulé ou si le colis est livré ou pas.
   if (req.body.status == "newMission") {
     deliveries = deliveries.filter((e) => e.isValidate == "notYet");
   } else if (req.body.status == "currentMission") {
-    deliveries = deliveries.filter(
-      (e) => e.isValidate == "accept" && e.delivery_status != "delivered"
+    deliveries = deliveries.filter((e) => e.isValidate == "accept" && e.delivery_status != "delivered"
     );
   } else if (req.body.status == "finishMission") {
     deliveries = deliveries.filter((e) => e.delivery_status == "delivered");
   }
 
-  
+ // pourcentage de la capacité detransport  
+  var etatCapacity = 100 - (mission.transport_capacity_rest * 100) / mission.transport_capacity_total;
 
-  var etatCapacity =
-    100 -
-    (mission.transport_capacity_rest * 100) / mission.transport_capacity_total;
-
-  console.log(etatCapacity);
-
+ // calcul de la cagnote
   var totalCagnotte = 0;
   deliveries.map((e) => (totalCagnotte += e.price));
 
@@ -334,6 +334,8 @@ router.post("/loadDeliveries", async function (req, res, next) {
 
   res.json({ result, etatCapacity, cagnotte: totalCagnotte });
 });
+
+
 
 router.post("/loadMyDeliveries", async function (req, res, next) {
   var deliveries = await deliveryModel.find({ expeditor_id: req.body.userId });
@@ -358,8 +360,6 @@ router.post("/loadMyDeliveries", async function (req, res, next) {
     }
   }
 
-  console.log("mydata", dbDeliveries);
-
   var result = false;
   if (deliveries) {
     result = true;
@@ -367,14 +367,25 @@ router.post("/loadMyDeliveries", async function (req, res, next) {
   res.json({ result, deliveries: dbDeliveries });
 });
 
+
+// change status 
+
+
 router.post("/changeStatusValidate", async function (req, res, next) {
+
   var mission = await missionModel.findById(req.body.idMission);
 
+  // si la capacité de transport est null , la mission n'est plus dispo lors de la recherche d'un kryer
   if (mission.transport_capacity_rest == 0) {
     mission.newMissionStatus = false;
   }
 
   var delivery = await deliveryModel.findById(req.body.idDelivery);
+
+  // si je suis dans le screen nouvelle mission , la mission devient dispo dans missions en cours, 
+  //et le colis a un status de demande accepté
+  //si la capcité de transport le permet alors je lui soustrait le poid du nouveau colis 
+  //sinon je renvoie une erreur a true qui est traité dans le front
 
   if (delivery.isValidate == "notYet") {
     mission.currentMissionStatus = true;
@@ -385,13 +396,14 @@ router.post("/changeStatusValidate", async function (req, res, next) {
       res.json({ err: true });
       return;
     }
+
+  // si je suis dans le screen  mission en cours  
   } else if (delivery.isValidate == "accept") {
     mission.finishMissionStatus = true;
     delivery.delivery_status = "delivered";
   }
 
   var missionSave = await mission.save();
-
   
   await delivery.save();
 
@@ -400,11 +412,13 @@ router.post("/changeStatusValidate", async function (req, res, next) {
     .populate("delivery_id")
     .exec();
 
+  // si dans une mission le nombre de colis accepté = le nombre de colis livré , 
+  //alors la mission est completement Accompli, elle n'est donc plus dispo, a part dans mission accompli
+
   if (
-    deliveries.delivery_id.filter((e) => e.delivery_status == "delivered")
-      .length ==
-    deliveries.delivery_id.filter((e) => e.isValidate == "accept").length
-  ) {
+    deliveries.delivery_id.filter((e) => e.delivery_status == "delivered").length ==
+    deliveries.delivery_id.filter((e) => e.isValidate == "accept").length)
+     {
     mission.currentMissionStatus = false;
     mission.newMissionStatus = false;
     missionSave = await mission.save();
@@ -415,6 +429,7 @@ router.post("/changeStatusValidate", async function (req, res, next) {
 
 
 router.post("/changeStatusCancel", async function (req, res, next) {
+
   var result = false;
   var delivery = await deliveryModel.findById(req.body.idDelivery);
 
@@ -422,6 +437,7 @@ router.post("/changeStatusCancel", async function (req, res, next) {
 
   var deliverySave = await delivery.save();
 
+  // lorsque le status du colis est cancel , je rajoute le poid du colis a la capacite de trnsp de la mission
   if (deliverySave.isValidate == "cancel") {
     var mission = await missionModel.findById(req.body.idMission);
 
@@ -440,7 +456,7 @@ router.post("/changeStatusTransit", async function (req, res, next) {
   var result = false;
   var delivery = await deliveryModel.findById(req.body.idDelivery);
   delivery.delivery_status = "inTransitDelivery";
-  var deliverySave = delivery.save();
+  var deliverySave = await delivery.save();
 
   if (deliverySave) {
     result = true;
@@ -448,7 +464,14 @@ router.post("/changeStatusTransit", async function (req, res, next) {
 
   res.json(result);
 });
+
+
+
+// message 
+
+
 router.post("/addMessageAccept", async function (req, res, next) {
+
   let newMessage = new messageModel({
     expeditor_id: req.body.expeditor,
     recipient_id: req.body.recipient,
@@ -462,19 +485,22 @@ router.post("/addMessageAccept", async function (req, res, next) {
   res.json({ result: true });
 });
 
+
+// recupere les differents interlocuteurs
 router.post("/loadLastMessage", async function (req, res, next) {
+
   var userId = await userModel.findOne({ token: req.body.token });
 
-  console.log(userId);
-
   /* get distinct destinataires*/
+  // on passe les Id en string pour mieux les manipuler puis on les repasse en objectId a la fin.
+
   var distinctDest1 = await messageModel
     .find({ recipient_id: userId._id })
     .distinct("expeditor_id");
   distinctDest1 = distinctDest1.toString().split(',');
   
  
-
+ 
   var distinctDest2 = await messageModel
     .find({ expeditor_id: userId._id })
     .distinct("recipient_id")
@@ -484,25 +510,19 @@ router.post("/loadLastMessage", async function (req, res, next) {
 
     distinctDest3 = [...distinctDest1,...distinctDest2];
     
+    // enleve les doublons si il y en a
     var distinctDest3String2 = [... new Set(distinctDest3)];
     
     var distinctDest = new Array(distinctDest3String2.length);
     
+    // enleve les chaine de caractere vide (c'est le cas lorqu'il n'y a un destinataire avec un seul message)
     distinctDest = distinctDest.filter(e=> e != '');
 
     for(var i=0; i<distinctDest3String2.length; i++){
-  
         distinctDest[i] = mongoose.Types.ObjectId(distinctDest3String2[i])
-      
-      
     }
 
-    console.log(distinctDest)
-    console.log(distinctDest3String2)
-
-    
-
-
+  // recherge des messages  avec chaque interlocuteur pour recuperer le dernier et l'afficher dans le front
   var messages = new Array(distinctDest.length);
   for (var i = 0; i < distinctDest.length; i++) {
    
@@ -525,8 +545,6 @@ router.post("/loadLastMessage", async function (req, res, next) {
     
    
     var destInfos = await userModel.find({ _id: distinctDest[i] });
-
-    
     
     messages[i] = {
       id_msg: msgExp[msgExp.length - 1]._id,
@@ -538,7 +556,7 @@ router.post("/loadLastMessage", async function (req, res, next) {
       timeStamp: msgExp[msgExp.length - 1].date,
     };
   }
-  //console.log(messages);
+
   var result = false;
   if (messages) {
     result = true;
@@ -546,6 +564,7 @@ router.post("/loadLastMessage", async function (req, res, next) {
   res.json({ result, messages });
 });
 
+// recupere les messages avec une personnes
 router.post("/loadMessages", async function (req, res, next) {
   var userId = await userModel.findOne({ token: req.body.token });
 
@@ -573,6 +592,7 @@ router.post("/loadMessages", async function (req, res, next) {
   res.json({ result, messages });
 });
 
+
 router.post("/sendMessage", async function (req, res, next) {
   let newMessage = new messageModel({
     expeditor_id: req.body.expeditor,
@@ -582,12 +602,11 @@ router.post("/sendMessage", async function (req, res, next) {
   });
 
   let messageSave = await newMessage.save();
-  console.log(messageSave);
   var result = "false";
   if (messageSave) {
     result = true;
   }
-  //console.log(newMessage);
+ 
   res.json({ result, newMessage });
 });
 
